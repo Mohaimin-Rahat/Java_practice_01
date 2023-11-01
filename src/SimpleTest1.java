9010d8cff455795908034d180df41da9cf9f70c8
Mohaimin-Rahat <amrahat99@gmail.com>
2023-11-01 15:59:35 -0400

    Java First Quiz

---
+++ src/SimpleTest1.java
@@ -0,0 +1,6 @@
+public class SimpleTest1 {                                      //This is public class
+
+    public static void main(String[] args) {                    //This is main method
+        System.out.println("I will be Java Hero one day");      //This line is used for print
+    }
+}
---
+++ src/SimpleTest2.java
@@ -0,0 +1,6 @@
+public class SimpleTest2 {
+
+    public static void main(String[] args) {                               //this is main method
+        System.out.println("I will practice Java day and night" );         // This is used for printing
+    }
+}
