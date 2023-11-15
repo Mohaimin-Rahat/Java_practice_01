07094d14e0fbd3d99410fb3825b3b04e188d3328
Abdul Mohaimin Rahat <47187464+Mohaimin-Rahat@users.noreply.github.com>
2023-11-15 16:40:15 -0500

    Delete Factorial.java

--- Factorial.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Factorial {
-    public int factorial(int number) {
-        if (number <= 1) return 1;
-        else return number * factorial(number - 1);
-    }
-}
