dbf8ab05147275319337c82d50292906747e0bc9
Abdul Mohaimin Rahat <47187464+Mohaimin-Rahat@users.noreply.github.com>
2023-11-15 16:41:58 -0500

    Delete PalindromChecker.java

--- PalindromChecker.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class PalindromChecker {
-
-    public boolean isPalindrome(String word) {
-        String reversed = new StringBuilder(word).reverse().toString();
-        return word.equals(reversed);
-    }
-}
